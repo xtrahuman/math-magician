ddc702cb7845bf49ec5f1435008b922ea2f8a9b9
unknown <okpara_tochukwu@yahoo.com>
2021-12-01 16:30:39 +0100

    added routers and styling

---
+++ src/pages/Quotes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quotes = () => (
+  <p className="Quotes">
+    Mathematics are the result of mysterious powers which no one
+    understands, and which the unconscious recognition of beauty must play
+    an important part. Out of an infinity of designs a mathematician chooses
+    one pattern for beauty’s sake and pulls it down to earth.
+  </p>
+);
+
+export default Quotes;
